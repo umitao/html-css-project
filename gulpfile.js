81cafa10d63e356bc2b9194cc8f1df9324843e49
umitao <umitao@gmail.com>
2020-03-26 02:19:51 +0300

    gulp & autoprefixer files

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+// var gulp = require("gulp");
+// var autoprefixer = require("gulp-autoprefixer");
+
+// gulp.task("styles", async function() {
+//   gulp
+//     .src("styles.css")
+//     .pipe(autoprefixer())
+//     .pipe(gulp.dest("build"));
+// });
+
+// const gulp = require("gulp");
+
+// gulp.task("taskname", function() {
+//   console.log("\nHello world!\n");
+// });
+
+// gulp.task('autoprefixer', () => {
+//   const autoprefixer = require('autoprefixer')
+//   const sourcemaps = require('gulp-sourcemaps')
+//   const postcss = require('gulp-postcss')
+
+//   return gulp.src('*.css')
+//     .pipe(sourcemaps.init())
+//     .pipe(postcss([ autoprefixer() ]))
+//     .pipe(sourcemaps.write('.'))
+//     .pipe(gulp.dest('./dest'))
+// })
+
+const autoprefixer = require("gulp-autoprefixer");
+const gulp = require("gulp");
+
+gulp.task("styles", () =>
+  gulp
+    .src("styles.css")
+    .pipe(
+      autoprefixer({
+        cascade: false
+      })
+    )
+    .pipe(gulp.dest("style"))
+);
